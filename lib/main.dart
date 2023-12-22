d9212f168843ee2f6a324105a9fa9a074838a73e
Arti Chaudhary <71622834+artichaudhary0@users.noreply.github.com>
2023-12-22 16:05:30 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,21 +35,111 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Text(
+          "Pick an image...",
+          style: TextStyle(
+            fontSize: 34,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.transparent,
+      extendBodyBehindAppBar: true,
       body: Center(
-        child: IconButton(
-          onPressed: () async {
-            XFile? image =
-                await ImagePicker().pickImage(source: ImageSource.camera);
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => EditTextScreen(
-                  selectedImage: image!.path.toString(),
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                "assets/blue.jpg",
+              ),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () async {
+                    XFile? image = await ImagePicker().pickImage(
+                      source: ImageSource.gallery,
+                    );
+                    // ignore: use_build_context_synchronously
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EditTextScreen(
+                          selectedImage: image!.path.toString(),
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.white),
+                      borderRadius: BorderRadius.circular(24),
+                      color: Colors.deepPurple.shade100,
+                    ),
+                    child: const Text(
+                      "Gallery",
+                      style: TextStyle(
+                        fontSize: 28,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                 ),
               ),
-            );
-          },
-          icon: const Icon(Icons.upload_file),
+              const SizedBox(width: 20),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () async {
+                    XFile? image = await ImagePicker().pickImage(
+                      source: ImageSource.camera,
+                    );
+                    // ignore: use_build_context_synchronously
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EditTextScreen(
+                          selectedImage: image!.path.toString(),
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.white),
+                      borderRadius: BorderRadius.circular(24),
+                      color: Colors.deepPurple.shade100,
+                    ),
+                    child: const Text(
+                      "Camera",
+                      style: TextStyle(
+                        fontSize: 28,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
